f6ff155753080df337ffce80671f52346d7a469d
Josh <joshfried97@gmail.com>
2020-07-13 16:34:18 +0100

    Minimal changes made

---
+++ Lyapunov_solver.m
@@ -0,0 +1,22 @@
+% Testing Lyapunov function in MATLAB 
+% Using jet engine example in slide 20 of Stanford slides
+clc
+clear all
+close all 
+
+% Following methodology using https://matlabexamples.wordpress.com/tag/lyapunov-stability/
+% as a reference.
+
+% Given dx/dt = -y - (3/2)x^2 - (1/2)x^3
+% Given dy/dt = 3x - y
+% Finding the equilibium point (x,y) = (0,0) means we can construct A and Q
+% as the following:
+A = [0 3 ; -1 -1];
+Q = eye(size(A));
+
+X = lyap(A, Q)
+
+% Find eigenvalues of output as this will determine stability
+k = eig(X)
+
+
--- SOSTOOL_Function_Runthrough.m
+++ SOSTOOL_Function_Runthrough.m
@@ -23,11 +23,23 @@ F = sos(p2);
 solvesos(F);
 h2 = sosd(F);
 
+% Testing FP accuracy with expression on slide 21
+p3 = 4.5819*x^2 - 1.5786*x*y + 1.7834*(y^2) - 0.12739*(x^3) + 2.5189*(x^2)*y - ...
+    0.34069*x*(y^2) + 0.61188*(y^3) + 0.47537*(x^4) - 0.052424*(x^3)*y + ...
+    0.44289*(x^2)*(y^2) + 0.0000018868*x*(y^3) + 0.090723*y^4;
+
+% Calculates and outputs decomp
+F = sos(p3);
+solvesos(F);
+h3 = sosd(F);
+
 fprintf("Calculation of p - h'*h to determine accuracy of SOS decomp:\n\n")
 
 fprintf("Stanford slides; slide 6 example:\n")
 p1-h1'*h1
 fprintf("\nStanford slides; slide 10 example:\n")
 p2-h2'*h2
+fprintf("\nStanford slides; slide 21 example:\n")
+p3-h3'*h3
 
 
