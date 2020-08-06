64a7b185a47cfc6694c0b1a343f128b83e5b2c47
Josh <joshfried97@gmail.com>
2020-08-06 14:38:56 +0100

    Added user input features such as entering #var #eqn and zeta val

---
+++ Amirs_Steps_Step1_and_2.m
@@ -0,0 +1,50 @@
+% Constructing algorithmic approach to satisfy that V and -grad(V)f_dot(x)
+% are both SOS.
+% Code found: http://control.asu.edu/Classes/MAE598/598Lecture16.pdf
+clc
+clear all
+close all
+
+% Prompt user for required data
+nVar = input('Enter number of variables in system: ');
+nEqn = input('Enter number of equations in system: ');
+
+% Set up YALMIP symbols
+x = sdpvar(nVar,1);
+
+% This structure contains each ODE per row
+f = [-1*x(2) - 1.5*x(1)^2 - 0.5*x(1)^3 ; 3*x(1) - x(2)];
+
+% Generates a polynomial with degree 4
+% V is polynomial
+% Vc stores coefficients of each term
+n = input('Enter degree polynomial to search for: ');
+[V, Vc] = polynomial(x,n);
+
+% Setting constant term to 0
+F = [Vc(1) == 0];
+
+% Generate boundary function for defining positive definiteness
+boundFun = 0;
+for i = 1 : nVar
+    boundFun = boundFun + x(i)^2;
+end
+
+% This ensures V is positive definite
+zeta = input('Enter value for zeta: ');
+F = [F;sos(V-zeta*boundFun^2)];
+
+% Calculates the gradient of the cost function
+gradV = jacobian(V,x);
+
+% Creates sos instance for both V and -V_dot
+F = [F;sos(-1*gradV*f)];
+
+% Calculates V s.t V and -V_dot are sos
+fprintf("\n\n")
+solvesos(F,[],[],[Vc]);
+
+% Construct V and display results
+V = replace(V,Vc,value(Vc))
+fprintf("V:\n")
+sdisplay(V)
