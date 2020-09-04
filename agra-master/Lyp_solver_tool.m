dd9b6a6708853225ee3c0e1c73a8c71c50773ae6
Josh <joshfried97@gmail.com>
2020-09-04 16:26:35 +0100

    Added functionality to loop through V degree until solution is found

--- agra-master/Lyp_solver_tool.m
+++ agra-master/Lyp_solver_tool.m
@@ -1,6 +1,5 @@
-% Constructing algorithmic approach to satisfy that V and -grad(V)f_dot(x)
+% Constructing algorithmic approach to satisfy that V and -V_dot
 % are both SOS.
-% Code found: http://control.asu.edu/Classes/MAE598/598Lecture16.pdf
 clc
 clear all
 close all
@@ -25,38 +24,30 @@ for i = 1 : nEqn
 end
 fprintf("\n")
 
-% Generates a polynomial with degree 4
-% V is polynomial
-% Vc stores coefficients of each term
-n = input('Enter degree polynomial to search for: ');
-[V, Vc] = polynomial(x,n);
-fprintf("\n")
+% Start searching for a quadratic V
+n = 2;
 
-% Generate boundary function for defining positive definiteness
-boundFun = sum(x.^2);
-zeta = 0.00001;
-
-% Calculates the gradient of the cost function
-gradV = jacobian(V,x);
-negGradVfDot = -1*gradV*f;
-
-% Creates sos instance for both V and -V_dot
-F = [sos(V-zeta*boundFun^2);sos(negGradVfDot)];
-
-% Calculates V s.t V and -V_dot are sos
-solvesos([F],[],[],[Vc]);
-
-% Cleans expression up by removing coefficients < 1e-6
-V = replace(V,Vc,value(Vc));
-V = clean(V, 1e-6);
-negGradVfDot = replace(negGradVfDot,Vc,value(Vc));
-negGradVfDot = clean(negGradVfDot, 1e-6);
-
-% Display V and negGradVfDot
-V = sdisplay(V);
-V = V{1}
-negGradVfDot = sdisplay(replace(negGradVfDot,Vc,value(Vc)));
-negGradVfDot = negGradVfDot{1}
+% MATLAB equivalent of do-while loop
+while 1
+  
+    % Run solver
+    fprintf("Searching for V with degree = %d...\n",n);
+    [V,neg_V_dot] = sosFun(f,x,n);
+    
+    % If the solver returns nothing then it's infeasible
+    if V == '0'
+        disp("")
+        disp("**** Infeasible solution -> Searching for higher order V ****")
+        disp("")
+        
+        % Increase the degree of V by 2
+        n = n + 2
+    else
+        % If the solver doesn't return nothing then it's found something
+        % and breaks loop
+        break;
+    end
+end
 
 % Utilising Agra toolbox for plotting ROA
 if (input('Do you want to plot ROA? (1 - Yes, 0 - No): '))
@@ -64,31 +55,72 @@ if (input('Do you want to plot ROA? (1 - Yes, 0 - No): '))
     Lyp_gui(f,V,nVar,nEqn)
 end
 
-% Plot V if it is plottable
+% Plot V and -V_dot if it is plottable
 if (nVar <= 2)
-    if (input('Do you want to plot V and -grad(V)f_dot? (1 - Yes, 0 - No): '))
-        V = replace(V,'*','.*');
-        V = replace(V,'^','.^');
-        V = replace(V,'x(1)','x');
-        V = replace(V,'x(2)','y');
-        V = eval(['@(x,y)' V]);
-        subplot(2,1,1);
-        fsurf(V)
-        title('Proposed Lyapunov Function')
-        xlabel("x")
-        ylabel("y")
-        zlabel("V")
-        
-        negGradVfDot = replace(negGradVfDot,'*','.*');
-        negGradVfDot = replace(negGradVfDot,'^','.^');
-        negGradVfDot = replace(negGradVfDot,'x(1)','x');
-        negGradVfDot = replace(negGradVfDot,'x(2)','y');
-        negGradVfDot = eval(['@(x,y)' negGradVfDot]);
-        subplot(2,1,2);
-        fsurf(negGradVfDot)
-        title('Negative Grad Lyapunov Function Multiplied by F')
-        xlabel("x")
-        ylabel("y")
-        zlabel("-grad(V)f")
+    if (input('Do you want to plot V and -V_dot? (1 - Yes, 0 - No): '))
+       plotFun(V, neg_V_dot)
     end
+end
+
+%% Function Declarations
+function plotFun(V, neg_V_dot)
+V = replace(V,'*','.*');
+V = replace(V,'^','.^');
+V = replace(V,'x(1)','x');
+V = replace(V,'x(2)','y');
+V = eval(['@(x,y)' V]);
+subplot(2,1,1);
+fsurf(V)
+title('Proposed Lyapunov Function')
+xlabel("x")
+ylabel("y")
+zlabel("V")
+
+neg_V_dot = replace(neg_V_dot,'*','.*');
+neg_V_dot = replace(neg_V_dot,'^','.^');
+neg_V_dot = replace(neg_V_dot,'x(1)','x');
+neg_V_dot = replace(neg_V_dot,'x(2)','y');
+neg_V_dot = eval(['@(x,y)' neg_V_dot]);
+subplot(2,1,2);
+fsurf(neg_V_dot)
+title('Negative Grad Lyapunov Function Multiplied by F')
+xlabel("x")
+ylabel("y")
+zlabel("-grad(V)f")
+end
+
+function [V,neg_V_dot] = sosFun(f,x,n)
+% Generates a polynomial with degree n
+% V is polynomial
+% Vc stores coefficients of each term
+[V, Vc] = polynomial(x,n);
+
+% Generate boundary function for defining positive definiteness
+boundFun = sum(x.^2);
+zeta = 0.00001;
+
+% Calculates the gradient of the cost function
+gradV = jacobian(V,x);
+neg_V_dot = -1*gradV*f;
+
+% Creates sos instance for both V and -V_dot
+F = [Vc(1) == 0;sos(V-zeta*boundFun^2);sos(neg_V_dot)];
+
+% Turning on pre & post processing to reduce computational load
+options = sdpsettings('sos.newton',1,'sos.congruence',1);
+
+% Calculates V s.t V and -V_dot are sos
+solvesos([F],[],options,[Vc]);
+
+% Cleans expression up by removing coefficients < 1e-6
+V = replace(V,Vc,value(Vc));
+V = clean(V, 1e-6);
+neg_V_dot = replace(neg_V_dot,Vc,value(Vc));
+neg_V_dot = clean(neg_V_dot, 1e-6);
+
+% Prepare expressions for displaying
+V = sdisplay(V);
+V = V{1}
+neg_V_dot = sdisplay(replace(neg_V_dot,Vc,value(Vc)));
+neg_V_dot = neg_V_dot{1}
 end
